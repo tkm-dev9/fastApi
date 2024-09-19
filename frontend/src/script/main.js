51d25ade78b2574a7d5668ef8269ae4e4ea90802
takumu1011 <sumikawa.web@gmail.com>
2024-09-20 00:45:57 +0900

    フォルダ構造変更

--- frontend/src/script/main.js
+++ frontend/src/script/main.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("btn");
   const output = document.getElementById("output");
+  
   btn.addEventListener("click", async () => {
-    const res = await fetch(__API_URL__);
+    const apiUrl = import.meta.env.VITE_API_URL;
+    if (!apiUrl) throw new Error('APIエンドポイントが設定されていません');
+    const res = await fetch(apiUrl);
     const data = await res.json();
     output.textContent = data.res;
   });
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from "vite";
-
-const apiUrl = process.env.API_URL;
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'view',
-  publicDir: '../script',
-  define: {
-    __API_URL__: JSON.stringify(apiUrl)
-  },
+  root: 'src',
+  envDir: 'config',
   server: {
     host: '0.0.0.0',
     port: 3000,
-  }
+    open: true,
+  },
 });
