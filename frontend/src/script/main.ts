15750ff6222b2950c560670754356f8a8c8fd15b
takumu1011 <sumikawa.web@gmail.com>
2024-09-21 22:46:08 +0900

    biomeの設定

--- frontend/src/script/main.ts
+++ frontend/src/script/main.ts
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
     console.error("DOMが見つかりませんでした。");
     return;
   }
-  
+
   btn.addEventListener("click", async () => {
     try {
       const apiUrl = import.meta.env.VITE_API_URL;
-      if (!apiUrl) throw new Error('APIエンドポイントが設定されていません');
+      if (!apiUrl) throw new Error("APIエンドポイントが設定されていません");
       const res = await fetch(apiUrl);
       if (!res.ok) throw new Error(`エラーが発生しました。: ${res.status}`);
       const data = await res.json();
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'src',
-  envDir: 'config',
+  root: "src",
+  envDir: "config",
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
     open: true,
   },
