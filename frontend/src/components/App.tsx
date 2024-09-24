d920d4b59bbaf8534f0dd1e12b06add1aa9b4028
takumu1011 <sumikawa.web@gmail.com>
2024-09-24 21:28:15 +0900

    react-router導入

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
 function App() {
   return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
